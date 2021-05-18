4fe64099bc14e9a695ed2c912cf789077dffe489
Laura del Toro <laura.deltoro@seddi.com>
2021-05-18 22:15:55 +0100

    Add first tests to AppointmentsDayView

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import {
+  Appointment,
+  AppointmentsDayView,
+} from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -10,7 +13,7 @@ describe('Appointment', () => {
     container = document.createElement('div');
   });
 
-  const render = component =>
+  const render = (component) =>
     ReactDOM.render(component, container);
 
   it('renders the customer first name', () => {
@@ -23,5 +26,47 @@ describe('Appointment', () => {
     customer = { firstName: 'Jordan' };
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
+  });
+});
+
+describe('AppointmentsDayView', () => {
+  let container;
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = (component) =>
+    ReactDOM.render(component, container);
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(
+      container.querySelector('div#appointmentsDayView')
+    ).not.toBeNull();
+  });
+
+  it('renders multiple appointments in an ol element', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  });
+  it('renders each appointment in an li element', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(
+      container.querySelectorAll('li')[0].textContent
+    ).toEqual('12:00');
+    expect(
+      container.querySelectorAll('li')[1].textContent
+    ).toEqual('13:00');
   });
 });
