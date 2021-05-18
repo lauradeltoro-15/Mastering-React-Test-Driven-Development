9abf0208896b036d1c8db3ff253fdbf4afcbd599
Laura del Toro <laura.deltoro@seddi.com>
2021-05-18 07:49:24 +0100

    First test passing

---
+++ appointments/test/Appointment.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+describe('Appointment', () => {
+  it('renders the customer first name', () => {
+    //GIVEN:
+    const customer = { firstName: 'Ashley' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    ReactDOM.render(component, container);
+
+    expect(document.body.textContent).toMatch('Ashley');
+  });
+  it('renders another customer first name', () => {
+    //GIVEN:
+    const customer = { firstName: 'Jordan' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    ReactDOM.render(component, container);
+
+    expect(document.body.textContent).toMatch('Jordan');
+  });
+});
